085634229364f81bb6d8002e7465972bf1e3c3bd
Rowan <rowantwell@gmail.com>
2023-09-11 21:18:10 +0100

    add nightcore labels

--- src/index.tsx
+++ src/index.tsx
@@ -185,7 +185,9 @@ const App: FunctionComponent = () => {
                   marks={[
                     { value: 0.1 },
                     { value: 0.5 },
+                    {value: 0.7334, label: "slow"},
                     { value: 1 },
+                    {value: 1.3636, label: "nightcore"},
                     { value: 1.5 },
                     { value: 2 },
                   ]}
